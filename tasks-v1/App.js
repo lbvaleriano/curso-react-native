dec884be1a42c17a5e6deb5bdd19e2b6b7ab97ea
lbvaleriano <lbvaleriano@gmail.com>
2020-02-04 20:46:09 -0300

    Novo Tasks
    Novo Tasks

---
+++ tasks-v1/App.js
@@ -0,0 +1,29 @@
+
+import React, { Component } from 'react'
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native'
+import commonStyles from './src/commonStyles'
+
+export default class App extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>Welcome to React Native</Text>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#f5fcff'
+	},
+	welcome: {
+		fontFamily: commonStyles.fontFamily,
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10
+	}
+})
--- tasks/index.js
+++ tasks/index.js
@@ -2,9 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native'
-import Agenda from './src/screens/Agenda'
-import Navigator from './src/Navigator'
-import {name as appName} from './app.json'
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Navigator);
+AppRegistry.registerComponent(appName, () => App);
